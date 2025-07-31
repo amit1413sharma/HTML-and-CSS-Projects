15c9bd6934026d6f65e879abff53dbf77ebea82f
amit1413sharma <amit1413sharma@gmail.com>
2025-07-31 03:44:09 -0400

    Project files

---
+++ Empty/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, World!';
+console.log(greeting);
+
+var greeting2 = 'Wlecome to Node.js';
+console.log(greeting2);
+
+var sentence1 = 'Learnig how to debug code with the debugger';
+console.log(sentenceOne);
